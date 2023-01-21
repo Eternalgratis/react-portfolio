5265662d2f91c8a4b74af7d1a611dd0a949dc47c
Titilope Apuabi <tyaps002@gmail.com>
2023-01-21 02:34:52 +0100

    Modify portfolio

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,25 +1,26 @@
-import React from 'react'
-import FullStack from '../../assets/fullstack.png'
-import HeaderSocials from './HeaderSocials'
-import CTA from './CTA'
-import './header.css'
+/* eslint-disable */
+import React from 'react';
+// import FullStack from '../../assets/fullstack.png';
+import HeaderSocials from './HeaderSocials';
+import CTA from './CTA';
+import './header.css';
 
 function Header() {
   return (
     <header>
       <div className="container header__container">
-        <h5>Hello  I&apos;m</h5>
-        <h1>Apuabi Titilope</h1>
-        {/* <h5 className='text-light'>FullStack Developer skilled in Javascript react ruby and ruby on rails</h5> */}
+        <h5 className="hello">Hello  I&apos;m</h5>
+        <h1 className="my-name">Apuabi Titilope</h1>
+        <p className="title">FullStack Developer</p>
         <CTA />
         <HeaderSocials />
         <div className="fullstack__image">
-          <img src={FullStack} alt="picure of me" id="slide" /> 
+          {/* <img src={FullStack} alt="picure of me" id="slide" /> */}
         </div>
-        <a href="#contact" className='scroll__down'>Scroll Down</a>
+        {/* <a href="#contact" className="scroll__down" target="_blank" rel="noreferrer">Scroll Down</a> */}
       </div>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
