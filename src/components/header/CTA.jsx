b3d2eb559063733a4dc2dfb4ba8517be6269a832
Titilope Apuabi <tyaps002@gmail.com>
2023-06-04 22:44:21 +0100

    Fix linter errors

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,13 +1,11 @@
-import React from 'react'
-import CV from '../../assets/newestResume.pdf'
+import React from 'react';
+import CV from '../../assets/newestResume.pdf';
 
-const CTA = () => {
-  return (
-    <div className='cta'>
-        <a href={CV} download className='btn'>Download CV </a>
-        <a href="#contact-segment" className='btn btn-primary'>Let&apos;s Talk</a>
-    </div>
-  )
-};
+const CTA = () => (
+  <div className="cta">
+    <a href={CV} download className="btn">Download CV </a>
+    <a href="#contact-segment" className="btn btn-primary">Let&apos;s Talk</a>
+  </div>
+);
 
-export default CTA
+export default CTA;
